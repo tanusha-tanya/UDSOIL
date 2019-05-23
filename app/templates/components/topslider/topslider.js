3449998f0fff8d93207a28788428d5458704aee9
tanusha-tanya <offchinnikova@bk.ru>
2019-05-23 18:39:18 +0400

    index started

---
+++ app/templates/components/topslider/topslider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {    
+    $('.topslider').owlCarousel({        
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        loop:true,
+        items:1,
+        smartSpeed:450,
+        nav: false     
+    });
+});
