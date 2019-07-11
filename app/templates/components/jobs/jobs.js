86676bc83ed251b3b8fecddf0017827b7fd24af2
tanusha-tanya <offchinnikova@bk.ru>
2019-07-11 18:32:51 +0400

    jobs

---
+++ app/templates/components/jobs/jobs.js
@@ -0,0 +1,12 @@
+const jobs = document.querySelector('.jobs');
+if(jobs){
+    let togglerCollection = document.querySelector('.jobs-openner');
+    togglerCollection.forEach((toggler)=>{
+        toggler.addEventListener('click', ()=>{
+            let parrent = toggler.closest('.jobs-item')
+            if(parrent.classList.contains){
+                
+            }
+        })
+    })
+}
--- app/templates/components/peopleslider/peopleslider.js
+++ app/templates/components/peopleslider/peopleslider.js
@@ -15,4 +15,38 @@ if(peopleslider){
             toggle.classList.toggle('controllers-toggler__right')
         })
     })
+    let slider = $('.peopleslider-slider')
+    if(slider){
+        if($(".peopleslider-slider-item").length > 1){
+            slider.owlCarousel({
+                items:1,
+                dots: false,
+                nav:true,
+                navContainer: '.peopleslider-arrows',
+                navElement: 'div class="peopleslider-arrow"',
+                navText: ['<svg role="img" width="11" height="14"><use xlink:href="#triangle"></use></svg>','<svg role="img" width="11" height="14"><use xlink:href="#triangle"></use></svg>']
+            }) 
+            let items = $('.peopleslider-slider .owl-item:not(.cloned)'),
+            total = items.length,
+            current = 1;
+            $('.peopleslider-counter').text(current +'/' + total);
+
+            slider.on('changed.owl.carousel', function(event) {
+                current = event.item.index + 1;
+                changeCurrent(current)
+            });  
+            function changeCurrent(current){
+                if(current === 0 ){
+                  current = total;
+                }
+                if (current > total){
+                  current = 1;
+                }                
+                $('.peopleslider-counter').text(current +'/' + total);
+              }
+        }
+        else{
+            $(".peopleslider-nav").remove()
+        }
+    }        
 }
