89c880233da7a40f0cd3e8192146bcb0dd4e5491
tanusha-tanya <offchinnikova@bk.ru>
2019-07-12 14:32:24 +0400

    jobs

--- app/templates/components/jobs/jobs.js
+++ app/templates/components/jobs/jobs.js
@@ -1,12 +1,22 @@
 const jobs = document.querySelector('.jobs');
 if(jobs){
-    let togglerCollection = document.querySelector('.jobs-openner');
-    togglerCollection.forEach((toggler)=>{
-        toggler.addEventListener('click', ()=>{
-            let parrent = toggler.closest('.jobs-item')
-            if(parrent.classList.contains){
-                
+    let openerCollection = document.querySelectorAll('.jobs-openner'),
+    jobsitems = document.querySelectorAll('.jobs-item')
+    openerCollection.forEach((opener)=>{
+        opener.addEventListener('click', ()=>{
+            let parrent = opener.closest('.jobs-item')
+            for(let i = 0; i < jobsitems.length; i++){
+                //console.log(jobsitems[i] !== parrent)
+                if(jobsitems[i] !== parrent){
+                    jobsitems[i].classList.remove('jobs-item__open')
+                }
+            }              
+            if(parrent.classList.contains('jobs-item__open')){
+                parrent.classList.remove('jobs-item__open')      
             }
-        })
+            else{
+                parrent.classList.add('jobs-item__open')
+            }
+        })        
     })
 }
