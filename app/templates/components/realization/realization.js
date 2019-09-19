ab642dbfee0b0549e5dbbb83ff71ceca9dc179a7
tanusha-tanya <offchinnikova@bk.ru>
2019-09-19 18:03:01 +0400

    realization

---
+++ app/templates/components/realization/realization.js
@@ -0,0 +1,9 @@
+$('.realization-partners-carousel').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,   
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+})
